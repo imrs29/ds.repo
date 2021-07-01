8b4fa29321730a760c5431a8dcf0e69928b75f19
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-07-01 20:09:52 +0530

    Add files via upload

---
+++ rev.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	int size = sizeof(arr)/sizeof(arr[0]);
+	for(int i=0;i<size;i++){
+		cin >> arr[i];
+	}
+
+	int start = 0 ,end = size-1;
+	while(start < end){
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end]  = temp;
+		start++;
+		end--;
+	}
+
+	for (int i = 0;i<size;i++){
+		cout << arr[i];
+	}
+}
