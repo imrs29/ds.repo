e159aa08857a6e8d4941d017dce0c791fd652450
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-07-02 00:03:11 +0530

    Update MaxMinElementArray.cpp

--- MaxMinElementArray.cpp
+++ MaxMinElementArray.cpp
@@ -1,2 +1,41 @@
-9
-2 5 9 3 1 12 6 8 7
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin >> arr[i];
+	}
+
+	int maxEle;
+	int minEle;
+	if(n == 1) // if there is only one element then max and min will be same
+	{
+		maxEle = arr[0];
+		minEle = arr[0];
+	}
+	else{
+		if(arr[0] > arr[1]){
+			maxEle = arr[0];
+			minEle = arr[1];
+		}
+		else{
+			maxEle = arr[1];
+			minEle = arr[0];
+		}
+		
+	
+		for(int i = 2;i<n;i++){
+			if(arr[i] > maxEle){
+			maxEle = arr[i];
+			}
+			if(arr[i] < minEle){
+			minEle = arr[i];
+			}
+		}
+	}
+	cout<<maxEle<<endl;
+	cout<<minEle<<endl;
+}
