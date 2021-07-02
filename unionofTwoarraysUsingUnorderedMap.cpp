6f2a258a0ad3196cba4575d5d4f636876f5d71a9
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-07-02 18:01:46 +0530

    Add files via upload

---
+++ unionofTwoarraysUsingUnorderedMap.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	unordered_map<int,int>mp;
+	int n,m;
+	cin >> n>>m;
+	int a[n],b[m];
+	for(int i = 0;i<n;i++){
+		cin>>a[i];
+		mp[a[i]]++;
+	}
+
+	for(int i =0;i < m; i++){
+		cin>>b[i];
+		mp[b[i]]++;
+	}
+	cout << mp.size() << endl;
+}
+
