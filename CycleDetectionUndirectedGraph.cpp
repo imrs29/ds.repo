7d975809ee864787b47bd39002ba07657130af83
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-07-18 23:00:56 +0530

    Add files via upload

---
+++ CycleDetectionUndirectedGraph.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* KEEP ON TRAVERSING THE GRAPH IF YOU FOUND AN EDGE POINTING TO AN ALREDAY VISITED NODE(EXCEPT THE PARENT NODE), A CYCLE IS FOUND. */
+bool iscycle(int src, vector<vector<int>> &adj, vector<bool> &visited,int parent){
+    visited[src] = true;
+    for(auto i  : adj[src]){
+        if(i != parent){
+            if(visited[i])
+                return true;
+            if(!visited[i] and iscycle(i,adj,visited,src)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+
+    int n , m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n);
+    for(int i = 0; i < m; i++){
+        int u , v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    bool cycle = false;
+    vector<bool> visited(n,false);
+    for(int i = 0; i < n; i++){
+        if(!visited[i] and iscycle(i,adj,visited,-1)){
+            cycle = true;
+        }
+    }
+    if(cycle)
+        cout << "Cycle is present";
+    else{
+        cout << " Cycle is not present";
+    }
+    return 0;
+}
