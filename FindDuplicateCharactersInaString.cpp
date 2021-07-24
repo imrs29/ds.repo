9e869b9851363e546a8bcf62a6809083a5307fe3
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-07-25 03:41:27 +0530

    Add files via upload

---
+++ FindDuplicateCharactersInaString.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+int main(){
+
+    string s;
+    cin >> s;
+
+    map<char,int>mp;
+
+    for(int i = 0; i < s.length(); i++){
+        mp[s[i]]++;
+    }
+
+    for(auto it : mp){
+        if(it.second > 1){
+            cout <<it.first<<" "<<"count = " << it.second << endl;
+        }
+    }
+}
