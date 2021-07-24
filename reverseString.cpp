b64412008503b2a1600c1843815dcf1235be34c8
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-07-25 03:04:30 +0530

    Update reverseString.cpp

--- reverseString.cpp
+++ reverseString.cpp
@@ -10,3 +10,33 @@ int main(){
 	    cout << str[i];
 	}
 }
+
+// 2nd approach
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+            
+            vector<char>v;
+            for(int i = s.size() - 1; i >= 0; i--){
+                    v.push_back(s[i]);
+            }
+            s = v;
+        
+    }
+};
+
+
+// 3rd approach 
+class Solution {
+public:
+void reverseString(vector& s) {
+int start=0,e=s.size()-1;
+while(start<e)
+{
+swap(s[start],s[e]);
+start++;
+e--;
+}
+}
+};
