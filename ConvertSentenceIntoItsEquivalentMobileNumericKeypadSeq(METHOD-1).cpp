7c8520e957bc1a5a074f37c48a97b84384ff801c
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-07-31 18:53:31 +0530

    Add files via upload

---
+++ ConvertSentenceIntoItsEquivalentMobileNumericKeypadSeq(METHOD-1).cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str[] = {"2" , "22" , "222" , "3" , "33" , "333" , "4" ,"44" , "444" , "5" , "55" , "555" , "6" , "66" , "666" , "7" , "77" ,"777" , "7777" , "8" , "88" , "888" , "9" , "99" , "999" , "9999"};
+
+    string s;
+    getline(cin,s);
+
+    string ans = "";
+    int n = s.length();
+    for(int i = 0; i < n; i++){
+        if(s[i]  == ' ') // if current element is space
+
+            ans += "0";
+        else{
+
+            int j = s[i] - 'A';
+            ans += str[j];
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+      
