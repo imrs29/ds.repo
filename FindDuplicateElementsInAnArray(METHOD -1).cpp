188311f914244e626a7018d16d04e9409bb4ffbe
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-08-02 12:32:38 +0530

    Add files via upload

---
+++ FindDuplicateElementsInAnArray(METHOD -1).cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+
+        // code here
+        vector<int> ans;
+        int f = 0;
+        for(int i  = 0; i < n; i++){
+            arr[arr[i] % n] = arr[arr[i] % n] + n;
+        }
+        
+        
+        for(int i = 0; i < n; i++){
+            if((arr[i]/n) > 1)
+            {
+            ans.push_back(i);
+            f = 1;
+            }
+            
+        }
+        
+        if(f == 0)
+        ans.push_back(-1);
+        return ans;
+        
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
