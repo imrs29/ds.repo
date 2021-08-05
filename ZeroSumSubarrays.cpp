268f2d6c706cee0fedda180e5d8ceddc2f4db780
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-08-05 17:28:43 +0530

    Add files via upload

---
+++ ZeroSumSubarrays.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    //Function to count subarrays with sum equal to 0.
+    ll findSubarray(vector<ll> arr, int n ) {
+        //code here
+        unordered_map<ll,ll>mp;
+        int sum = 0;
+        int c = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            if(sum == 0)
+            c++;
+            c += mp[sum];
+            mp[sum]++;
+        }
+        return c;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n; //input size of array
+       
+        vector<ll> arr(n,0);
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i]; //input array elements
+        Solution ob;
+        cout << ob.findSubarray(arr,n) << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
