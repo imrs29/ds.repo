ab64816d388df60f93bb40d811262c80b4f052f4
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-08-05 12:37:56 +0530

    Add files via upload

---
+++ CountTripletSortingOptimalApproach.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    
+    
+    public:
+    long long countTriplets(long long arr[], int n, long long sum)
+    {
+        // Your code goes here
+        sort(arr, arr + n);
+        int ans = 0;
+        for(int i = 0; i < n - 2; i++){
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right){
+                int check = arr[i] + arr[left] + arr[right];
+                if(check < sum){
+                    ans += (right - left);
+                    left++;
+                }
+                else right--;
+            }
+        }
+        return ans;
+    }
+         
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+  
+   
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        long long sum;
+        cin>>n>>sum;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+
+       
+
+        Solution ob;
+        cout << ob.countTriplets(arr, n, sum) ;
+       
+        
+        cout << "\n";
+         
+    }
+    return 0;
+}
+
+// o(n^2)
