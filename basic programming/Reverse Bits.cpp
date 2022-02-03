c8ace547c5fa5b09510b643940d32e1ad60848c5
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-02-03 22:11:01 +0530

    Create Reverse Bits.cpp

---
+++ basic programming/Reverse Bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 1;
+        for(int i=0; i < 32; i++){
+            res = res << 1;
+            if(n & 1 == 1) 
+                res++;
+            n = n >> 1;
+        }
+        return res;
+    }
+};
+
+// Reverse bits using bit manipulation
