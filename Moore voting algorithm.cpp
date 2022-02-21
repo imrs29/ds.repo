ed5a2b37d617c30cac45e93cd947d88126399b6d
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-02-21 14:37:43 +0530

    Create Moore voting algorithm.cpp

---
+++ Moore voting algorithm.cpp
@@ -0,0 +1,43 @@
+// *** Moore voting algorithm ***//
+/* Finding majority element 
+ 2  3  4  3  3 */
+
+#include <bits/stdc++.h>
+using namespace std;
+// Function to find majority element
+int findMajority(int arr[], int n)
+{
+	int maj_index = 0, count = 1;
+	// Finding majority candidate
+	for (int i = 0; i < n; i++) {
+		if (count == 0) {
+			maj_index = arr[i];
+			count = 1;
+		}
+		else {
+			if (arr[i] == maj_index)
+				count++;
+			else
+				count--;
+		}
+	}
+	int ct = 0;
+	// Checking if majority candidate occurs more than n/2
+	// times
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == maj_index)
+			ct++;
+	}
+
+	if (ct > n / 2)
+		return maj_index;
+	return -1;
+}
+int main()
+{
+	int arr[] = { 1, 1, 1, 1, 2, 3, 4 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int majority = findMajority(arr, n);
+	cout << " The majority element is : " << majority;
+	return 0;
+}
