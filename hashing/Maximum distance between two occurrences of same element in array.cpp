be8b4a08da4ebe33bbbcd17391ed700663b87965
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-03-02 22:36:22 +0530

    Create Maximum distance between two occurrences of same element in array.cpp

---
+++ hashing/Maximum distance between two occurrences of same element in array.cpp
@@ -0,0 +1,35 @@
+//maximum distance between two occurrences of same element in array
+
+// example : arr[] = {3,2,1,2,1,4,5,8,6,7,4,2}
+// output : 10
+// maximum distance for 2 is 11-1 = 10
+//pick the elements from the array and find its first and last index
+//O(n^2)
+
+//efficient solution is to use hashing technique
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    unordered_map<int,int>mp;
+    int max_distance = 0;
+    for(int i = 0; i < n; i++){
+        
+        if(mp.find(arr[i]) == mp.end())
+            mp[arr[i]] = i;
+
+        else
+            max_distance = max(max_distance, i - mp[arr[i]]);
+    }
+    return max_distance;
+}
