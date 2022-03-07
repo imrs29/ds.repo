ee3916e72a808458b03c97eb3a798ffd6a0a323d
imrs29 <74778063+imrs29@users.noreply.github.com>
2022-03-07 23:02:02 +0530

    Update Maximum distance between two occurrences of same element in array.cpp

--- hashing/Maximum distance between two occurrences of same element in array.cpp
+++ hashing/Maximum distance between two occurrences of same element in array.cpp
@@ -33,3 +33,39 @@ int main()
     }
     return max_distance;
 }
+
+// hashing
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+   
+   unordered_map<int, int> previous_Sum;
+   
+   int ans = 0, currSum = 0;
+   
+   for(int i = 0; i < n; i++){
+       currSum += arr[i];
+       
+       if(currSum == k)
+       ans++;
+       
+       if(previous_Sum.find(currSum - k) != previous_Sum.end())
+        ans += (previous_Sum[currSum - k]);
+        
+        previous_Sum[currSum]++;
+   }
+   cout <<ans;
+   
+   
+}
